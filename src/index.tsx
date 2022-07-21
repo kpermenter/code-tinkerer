48856f389269a269416557093cab5b0338bd0c95
kpermenter <52431178+kpermenter@users.noreply.github.com>
2022-07-21 17:23:32 -0500

    Add files via upload

--- src/index.tsx
+++ src/index.tsx
@@ -1,31 +1,36 @@
+import 'bulmaswatch/superhero/bulmaswatch.min.css';
 import { StrictMode, useState, useEffect, useRef } from 'react';
 import { createRoot } from "react-dom/client";
-import * as esbuild from 'esbuild-wasm';
+import * as esbuild from "esbuild-wasm";
 import { unpkgPathPlugin } from './plugins/unpkg-path-plugin';
 import { fetchPlugin } from './plugins/fetch-plugin';
+import { CodeEditor } from './components/codeEditor';
 
-const App = () => {
-	const ref = useRef<any>();
+const App: React.FC = () => {
+	const serviceRef = useRef<any>();
+	const iframe = useRef<any>();
 	const [input, setInput] = useState('');
-	const [code, setCode] = useState('');
 
 	const startService = async () => {
-		ref.current = await esbuild.startService({
-			worker: true,
-			wasmURL: 'https://unpkg.com/esbuild-wasm@0.8.27/esbuild.wasm'
-		});
-	};
-
+		serviceRef.current = await esbuild.startService(
+			{
+				worker: true,
+				wasmURL: "https://unpkg.com/esbuild-wasm@0.8.27/esbuild.wasm"
+			}
+		);
+	}
 	useEffect(() => {
 		startService();
 	}, []);
 
 	const onClick = async () => {
-		if (!ref.current) {
+		if (!serviceRef.current) {
 			return;
 		}
 
-		const result = await ref.current.build({
+		iframe.current.srcdoc = html;
+
+		const result = await serviceRef.current.build({
 			entryPoints: ['index.js'],
 			bundle: true,
 			write: false,
@@ -34,36 +39,53 @@ const App = () => {
 				fetchPlugin(input)
 			],
 			define: {
-				'process.env.NODE.ENV': '"production"',
+				"process.env.NODE_ENV": '"production"',
 				global: 'window'
-			},
-		});
+			}
+		})
 
-		setCode(result.outputFiles[0].text);
-	};
+		iframe.current.contentWindow.postMessage(result.outputFiles[0].text, '*')
+	}
 
 	const html = `
-	<script>
-	  ${code}
-	</script>
-	`;
-
+        <html>
+        <head></head>
+        <body>
+            <div id="root"></div>
+            <script>
+                window.addEventListener('message', (event) => {
+                try {
+                  eval(event.data);
+                } catch (err){
+                  const root = document.querySelector("#root");
+                  root.innerHTML = '<div style = "color: red;" ><h4>RunTimeError:</h4>' + err + '</div>';
+                  console.log(err);
+                }
+                }, false);
+            </script>
+        </body>
+        </html>
+    `
 	return (
 		<div>
-		<textarea value={input} onChange={e => setInput(e.target.value)}></textarea>
-		<div>
+			<div>
+				<CodeEditor
+					initialValue={''}
+					onChange={(value) => setInput(value)} />
+				<textarea value={input} style={{color: 'red'}} onChange={(e) => setInput(e.target.value)}></textarea>
+			</div>
 			<button onClick={onClick}>Submit</button>
-		</div>
-			<pre>{code}</pre>
-			<iframe sandbox='allow-scripts' title='html' srcDoc={html} />
+			<br />
+			<div>
+				<iframe title="preview" ref={iframe} sandbox="allow-scripts" srcDoc={html} />
+			</div>
 		</div>
 	);
-};
+}
 
 const root = createRoot(document.getElementById("root") as HTMLElement);
 root.render(
-  <StrictMode>
-    <App />
-  </StrictMode>
+	<StrictMode>
+		<App />
+	</StrictMode>
 );
-
